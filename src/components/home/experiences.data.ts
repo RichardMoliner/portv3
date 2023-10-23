e8fa65138ad364144521eec381113002ce24035e
Daiana Cambruzzi Ávila <DAIANA.AVILA@GMAIL.COM>
2023-10-23 20:44:45 -0300

    Update experiences.data.ts

--- src/components/home/experiences.data.ts
+++ src/components/home/experiences.data.ts
@@ -2,18 +2,23 @@ import { IExperience } from '@/interfaces/experiences';
 
 const experiencesData: IExperience[] = [{
   id: 1,
+  title: 'TITLE_SOFTWARE_ENGINEERING_MANAGER',
+  company: 'COMPANY_SOFTWARE_ENGINEERING_MANAGER',
+  description: 'DESCRIPTION_SOFTWARE_ENGINEERING_MANAGER'
+},{
+  id: 2,
   title: 'TITLE_TECH_MANAGER',
   company: 'COMPANY_TECH_MANAGER',
   description: 'DESCRIPTION_TECH_MANAGER'
 },
 {
-  id: 2,
+  id: 3,
   title: 'TITLE_SOFTWARE_ARCHITECT',
   company: 'COMPANY_SOFTWARE_ARCHITECT',
   description: 'DESCRIPTION_SOFTWARE_ARCHITECT'
 },
 {
-  id: 3,
+  id: 4,
   title: 'TITLE_SENIOR_FRONTEND',
   company: 'COMPANY_SENIOR_FRONTEND',
   description: 'DESCRIPTION_SENIOR_FRONTEND'
